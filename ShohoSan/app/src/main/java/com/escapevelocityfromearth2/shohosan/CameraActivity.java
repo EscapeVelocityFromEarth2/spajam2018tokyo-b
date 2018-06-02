0a1638a9cf81175603a93d40dfffa73587bf30df
池田 遼介 <ryosuke.ikeda@brilliantservice.co.jp>
2018-06-02 23:19:08 +0900

    OCR実装：：JSON受け取りまで

--- ShohoSan/app/src/main/java/com/escapevelocityfromearth2/shohosan/CameraActivity.java
+++ ShohoSan/app/src/main/java/com/escapevelocityfromearth2/shohosan/CameraActivity.java
@@ -10,8 +10,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.media.ImageReader;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -27,12 +27,31 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpTransport;
+
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.vision.v1.Vision;
+import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.EntityAnnotation;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -41,9 +60,14 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     private Camera2 mCamera2;
     private int REQUEST_CODE_CAMERA_PERMISSION = 1;
     private Button shtBtn;
+    private Button okButton;
+    private Button cancelButton;
+
     private int REQUEST_GALLERY = 0;
     private Context mContext = this;
     private int REQUEST_CODE_STORAGE_PERMISSION = 1;
+
+    private Bitmap mBitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +75,10 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_camera);
         shtBtn = (Button)findViewById(R.id.shutter_btn);
         shtBtn.setOnClickListener(this);
+        okButton = (Button)findViewById(R.id.ok_button);
+        okButton.setOnClickListener(this);
+        cancelButton = (Button)findViewById(R.id.cancel_button);
+        cancelButton.setOnClickListener(this);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             requestCameraPermissions();
@@ -94,28 +122,134 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         return super.onKeyDown(keyCode, event);
     }
 
-
     @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onClick(View view) {
-        mCamera2.takePicture(new ImageReader.OnImageAvailableListener() {
-            @Override
-            public void onImageAvailable(ImageReader reader) {
-                // 撮れた画像をImageViewに貼り付けて表示。
-                final Image image = reader.acquireLatestImage();
-                ByteBuffer buffer = image.getPlanes()[0].getBuffer();
-                byte[] bytes = new byte[buffer.remaining()];
-                buffer.get(bytes);
-                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                image.close();
-                saveImage(bitmap);
-                mImageView.setImageBitmap(bitmap);
-                mImageView.setVisibility(View.VISIBLE);
-                mTextureView.setVisibility(View.INVISIBLE);
-            }
-        });
+        switch(view.getId()) {
+            case R.id.shutter_btn:
+                mCamera2.takePicture(new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        // 撮れた画像をImageViewに貼り付けて表示。
+                        final android.media.Image image = reader.acquireLatestImage();
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.remaining()];
+                        buffer.get(bytes);
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        image.close();
+                        mBitmap = bitmap;
+                        saveImage(bitmap);
+                        mImageView.setImageBitmap(bitmap);
+                        mImageView.setVisibility(View.VISIBLE);
+                        mTextureView.setVisibility(View.INVISIBLE);
+                    }
+                });
+                break;
+            case R.id.ok_button:
+                try {
+                    callCloudVision(mBitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.cancel_button:
+                break;
+
+            default:
+                break;
+        }
 
     }
+
+    static private void callCloudVision(final Bitmap bitmap) throws IOException {
+
+        // Do the real work in an async task, because we need to use the network anyway
+        new AsyncTask<Object, Void, String>() {
+            @Override
+            protected String doInBackground(Object... params) {
+                try {
+
+                    HttpTransport httpTransport = AndroidHttp.newCompatibleTransport();
+                    JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+
+                    Vision.Builder builder = new Vision.Builder(httpTransport, jsonFactory, null);
+                    builder.setVisionRequestInitializer(new VisionRequestInitializer("AIzaSyBgYO94PqGDESiNwiLLQQWPJHfkDFAsPWk"));
+                    builder.setApplicationName("shohosan");
+                    Vision vision = builder.build();
+
+                    BatchAnnotateImagesRequest batchAnnotateImagesRequest =
+                            new BatchAnnotateImagesRequest();
+                    batchAnnotateImagesRequest.setRequests(new ArrayList<AnnotateImageRequest>() {{
+                        AnnotateImageRequest annotateImageRequest = new AnnotateImageRequest();
+                        // Add the image
+                        Image base64EncodedImage = new Image();
+                        // Convert the bitmap to a JPEG
+                        // Just in case it's a format that Android understands but Cloud Vision
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream);
+                        byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+                        // Base64 encode the JPEG
+                        base64EncodedImage.encodeContent(imageBytes);
+                        annotateImageRequest.setImage(base64EncodedImage);
+
+                        // add the features we want
+                        annotateImageRequest.setFeatures(new ArrayList<Feature>() {{
+                            Feature labelDetection = new Feature();
+                            labelDetection.setType("TEXT_DETECTION");
+                            labelDetection.setMaxResults(10);
+                            add(labelDetection);
+                        }});
+
+                        // Add the list of one thing to the request
+                        add(annotateImageRequest);
+                    }});
+
+                    Vision.Images.Annotate annotateRequest =
+                            vision.images().annotate(batchAnnotateImagesRequest);
+                    // Due to a bug: requests to Vision API containing large images fail when GZipped.
+                    annotateRequest.setDisableGZipContent(true);
+                    Log.d("CameraActivity", "created Cloud Vision request object, sending request");
+
+                    BatchAnnotateImagesResponse response = annotateRequest.execute();
+                    System.out.println(convertResponseToString(response));
+                    Log.d("CameraActivity", "reqest recieved " + response);
+                    return convertResponseToString(response);
+
+                } catch (GoogleJsonResponseException e) {
+                    Log.d("CameraActivity", "failed to make API request because " + e.getContent());
+                } catch (IOException e) {
+                    Log.d("CameraActivity", "failed to make API request because of other IOException " +
+                            e.getMessage());
+                }
+                return "Cloud Vision API request failed. Check logs for details.";
+            }
+
+            protected void onPostExecute(String result) {
+                Log.d("CameraActivity", "Congrats! Request Recieved!!!!!");
+                //mImageDetails.setText(result);
+
+            }
+        }.execute();
+    }
+
+    private static String convertResponseToString(BatchAnnotateImagesResponse response) {
+        String message = "I found these Text How was it?:\n\n";
+
+        List<EntityAnnotation> labels = response.getResponses().get(0).getTextAnnotations();
+        if (labels != null) {
+            for (EntityAnnotation label : labels) {
+                message += String.format("%.3f: %s", label.getScore(), label.getDescription());
+                message += "\n";
+
+            }
+        } else {
+            message += "nothing";
+        }
+
+        return message;
+    }
+
 
     private void saveImage(Bitmap image){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
